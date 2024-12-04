b0a58c5d8c4dbaa41aad288034c804d77e89417f
Jerome White <jerome@thewex.net>
2024-12-04 15:40:04 +0530

    Overwrite existing files only if desired

--- src/_marker/run.py
+++ src/_marker/run.py
@@ -10,18 +10,23 @@ if __name__ == '__main__':
     arguments = ArgumentParser()
     arguments.add_argument('--source', type=Path)
     arguments.add_argument('--destination', type=Path)
+    arguments.add_argument('--overwrite', action='store_true')
     args = arguments.parse_args()
 
     artifact_dict = create_model_dict()
     converter = PdfConverter(artifact_dict=artifact_dict)
 
     for i in args.source.rglob('*.pdf'):
-        dst = (args
-               .destination
-               .joinpath(i.relative_to(args.source))
-               .with_suffix('.md'))
-        dst.parent.mkdir(parents=True, exist_ok=True)
+        source = i.relative_to(args.source)
+        destination = (args
+                       .destination
+                       .joinpath(source)
+                       .with_suffix('.md'))
+        if destination.exists() and not args.overwrite:
+            logging.error('%s exists (skipping)', source)
+            continue
+        destination.parent.mkdir(parents=True, exist_ok=True)
 
-        logging.warning('%s -> %s', i, dst)
+        logging.warning('%s -> %s', i, destination)
         (text, *_) = text_from_rendered(converter(str(i)))
-        dst.write_text(text)
+        destination.write_text(text)
