308f0a35cdd04095801adc8a03b7dbd35010a0c2
Jerome White <jerome@thewex.net>
2024-12-12 15:32:49 +0530

    Changes to scoring
    * Use cosine similarity instead of the dot product. Although the
      rankings remain the same, the interpretation of the results is more
      straightforward.
    
    * Numpy replaced with Scipy for similarity calculations.

--- src/evaluate/openai-pairwise/score.py
+++ src/evaluate/openai-pairwise/score.py
@@ -1,12 +1,13 @@
 import math
 import json
 import itertools as it
+import statistics as st
 import collections as cl
 from pathlib import Path
 from argparse import ArgumentParser
 from dataclasses import dataclass, asdict, astuple
 
-import numpy as np
+from scipy import spatial
 
 from utils import Paragraph
 
@@ -36,7 +37,7 @@ def reader(path):
             kwargs = json.loads(row)
             yield Paragraph(**kwargs)
 
-def distances(source, target):
+def distances(source, target, delimiter='-'):
     keys = (
         'source',
         'target',
@@ -44,10 +45,14 @@ def distances(source, target):
 
     iterable = map(reader, (source, target))
     for (lhs, rhs) in it.product(*iterable):
-        # https://help.openai.com/en/articles/6824809-embeddings-frequently-asked-questions
-        dist = np.dot(lhs.data, rhs.data)
-        args = (f'{x}-{y}' for (x, y) in zip(keys, (lhs.order, rhs.order)))
+        # https://platform.openai.com/docs/guides/embeddings/how-can-i-retrieve-k-nearest-embedding-vectors-quickly#which-distance-function-should-i-use
+        dist = 1 - spatial.distance.cosine(lhs.data, rhs.data)
 
+        values = (
+            lhs.order,
+            rhs.order,
+        )
+        args = (f'{x}{delimiter}{y}' for (x, y) in zip(keys, values))
         yield Distance(*args, dist)
 
 #
@@ -59,29 +64,32 @@ if __name__ == '__main__':
     arguments.add_argument('--target', type=Path)
     args = arguments.parse_args()
 
-    pairs = sorted(distances(args.source, args.target))
+    _delimiter = '-'
+
+    values = distances(args.source, args.target, _delimiter)
+    pairs = sorted(values, reverse=True)
     keys = it.chain.from_iterable(map(list, pairs))
-    nodes = dict(zip(keys, it.repeat(False)))
+    nodes = dict(zip(keys, it.repeat(True)))
 
     #
     #
     #
     values = []
     for p in pairs:
-        if not any(map(nodes.get, p)):
+        if all(map(nodes.get, p)):
             values.append(p.distance)
             for k in p:
-                nodes[k] = True
-    score = np.mean(values)
+                nodes[k] = False
+    score = st.fmean(values)
 
     #
     #
     #
     counts = cl.defaultdict(list)
     for (k, v) in nodes.items():
-        key = k[:3]
-        counts[key].append(int(v))
-    coverage = { x: np.mean(y) for (x, y) in counts.items() }
+        (key, _) = k.split(_delimiter, maxsplit=1)
+        counts[key].append(1 - v) # since unseen is True
+    coverage = { x: st.fmean(y) for (x, y) in counts.items() }
 
 
     #
