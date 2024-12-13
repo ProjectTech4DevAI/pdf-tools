f4983ea26baeca7e55b2dc80db8c8c6395a57fe0
Jerome White <jerome@thewex.net>
2024-12-13 11:00:21 +0530

    Treat scoring as an assignment problem
    The objective is to maximize cosine similarity between source and
    target paragraphs.

--- src/evaluate/openai-pairwise/score.py
+++ src/evaluate/openai-pairwise/score.py
@@ -1,62 +1,21 @@
-import math
 import json
-import itertools as it
-import statistics as st
-import collections as cl
 from pathlib import Path
 from argparse import ArgumentParser
-from dataclasses import dataclass, asdict, astuple
 
-from scipy import spatial
+from scipy.optimize import linear_sum_assignment
+from sklearn.metrics.pairwise import cosine_similarity
 
 from utils import Paragraph
 
-@dataclass
-class Distance:
-    src: str
-    dst: str
-    distance: float
-
-    def __iter__(self):
-        yield from it.islice(astuple(self), 2)
-
-    def __lt__(self, other):
-        return self.distance < other.distance
-
-@dataclass
-class Result:
-    score: float
-    confusion: dict
-
-#
-#
-#
-def reader(path):
+def scanf(path):
     with path.open() as fp:
         for row in fp:
-            kwargs = json.loads(row)
-            yield Paragraph(**kwargs)
-
-def distances(source, target, delimiter='-'):
-    keys = (
-        'source',
-        'target',
-    )
-
-    iterable = map(reader, (source, target))
-    for (lhs, rhs) in it.product(*iterable):
-        # https://platform.openai.com/docs/guides/embeddings/how-can-i-retrieve-k-nearest-embedding-vectors-quickly#which-distance-function-should-i-use
-        dist = 1 - spatial.distance.cosine(lhs.data, rhs.data)
-
-        values = (
-            lhs.order,
-            rhs.order,
-        )
-        args = (f'{x}{delimiter}{y}' for (x, y) in zip(keys, values))
-        yield Distance(*args, dist)
+            para = Paragraph(**json.loads(row))
+            yield para.data
 
 #
-#
+# Using cosine similarity is appropriate:
+# https://platform.openai.com/docs/guides/embeddings/how-can-i-retrieve-k-nearest-embedding-vectors-quickly#which-distance-function-should-i-use
 #
 if __name__ == '__main__':
     arguments = ArgumentParser()
@@ -64,36 +23,15 @@ if __name__ == '__main__':
     arguments.add_argument('--target', type=Path)
     args = arguments.parse_args()
 
-    _delimiter = '-'
+    (source, target) = (list(scanf(x)) for x in (args.source, args.target))
+    ratio = len(source) / len(target)
 
-    values = distances(args.source, args.target, _delimiter)
-    pairs = sorted(values, reverse=True)
-    keys = it.chain.from_iterable(map(list, pairs))
-    nodes = dict(zip(keys, it.repeat(True)))
+    similarity = cosine_similarity(source, target)
+    (r, c) = linear_sum_assignment(similarity, maximize=True)
+    score = similarity[r, c].mean()
 
-    #
-    #
-    #
-    values = []
-    for p in pairs:
-        if all(map(nodes.get, p)):
-            values.append(p.distance)
-            for k in p:
-                nodes[k] = False
-    score = st.fmean(values)
-
-    #
-    #
-    #
-    counts = cl.defaultdict(list)
-    for (k, v) in nodes.items():
-        (key, _) = k.split(_delimiter, maxsplit=1)
-        counts[key].append(1 - v) # since unseen is True
-    coverage = { x: st.fmean(y) for (x, y) in counts.items() }
-
-
-    #
-    #
-    #
-    result = Result(score, coverage)
-    print(json.dumps(asdict(result), indent=3))
+    result = {
+        'score': score,
+        'st-ratio': ratio,
+    }
+    print(json.dumps(result, indent=3))
