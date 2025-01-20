8253755cfd33f1590adf381c5a73db6ed2e74c8b
Jerome White <jerome@thewex.net>
2025-01-20 11:44:19 +0530

    Use GPT-4o by default

--- src/build/_zerox/run.py
+++ src/build/_zerox/run.py
@@ -43,7 +43,7 @@ def jobs(args):
 
 if __name__ == '__main__':
     arguments = ArgumentParser()
-    arguments.add_argument('--model', default='gpt-4o-mini')
+    arguments.add_argument('--model', default='gpt-4o')
     arguments.add_argument('--source', type=Path)
     arguments.add_argument('--destination', type=Path)
     arguments.add_argument('--overwrite', action='store_true')
