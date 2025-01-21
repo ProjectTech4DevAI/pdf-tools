ae6408a34f9e6346233679280e5fac39eb87fc6c
Jerome White <jerome@thewex.net>
2025-01-21 13:21:10 +0530

    End-to-end scripting of AWS PDFs for Github Markdowns

---
+++ bin/aws-to-github.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+ROOT=`git rev-parse --show-toplevel`
+
+_workflows=(
+    zerox/openai_gpt-4o
+    # marker/default
+)
+
+while getopts 's:d:b:h' option; do
+    case $option in
+        s) _pdfs="$OPTARG" ;;
+        d) _markdowns=$OPTARG ;;
+	b) _bucket=$OPTARG ;;
+        h)
+            cat <<EOF
+Usage: $0
+ -s Local source directory containing PDFs (optional)
+ -d Github repository for Markdown destination
+ -b AWS bucket containing PDFs (must have s3:// prefix)
+EOF
+            exit 0
+            ;;
+        *)
+            echo -e Unrecognized option \"$option\"
+            exit 1
+            ;;
+    esac
+done
+
+if [ ! $_pdfs ]; then
+    _pdfs=`mktemp --directory`
+fi
+
+aws s3 sync $_bucket $_pdfs/ --no-progress --delete
+for i in ${_workflows[@]}; do
+    dst=$_markdowns/$i
+    model=$(dirname $i)
+
+    find $dst -name '*.md' \
+	| while read; do
+	md=$(realpath --relative-to=$dst "$REPLY")
+	src="$_pdfs/$md"
+	src=$(sed -e's/\.md/.pdf/' <<< "$src")
+	if [ ! -f "$src" ]; then
+	    (cd $dst && git rm "$md")
+	fi
+    done
+    $ROOT/bin/run.sh -s $_pdfs -d $_markdowns -m $model
+
+    (cd $_markdowns \
+	 && git add $i \
+	 && git commit -m "Document add/removal using $model"
+    )
+done
